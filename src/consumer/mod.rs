94daaf73dc290bd4109942c9af85346b68f83fe8
Pedro <pedrobeethoven8@gmail.com>
2026-04-10 18:26:33 -0300

    Refactor consume method to accept job by reference for improved performance

--- src/consumer/mod.rs
+++ src/consumer/mod.rs
@@ -2,13 +2,13 @@ use crate::error::QueueError;
 use crate::queue::models;
 
 pub trait Consumer {
-    fn consume(&self, job: models::Job) -> Result<(), QueueError>;
+    fn consume(&self, job: &models::Job) -> Result<(), QueueError>;
 }
 
 pub struct JobConsumer;
 
 impl Consumer for JobConsumer {
-    fn consume(&self, job: models::Job) -> Result<(), QueueError> {
+    fn consume(&self, job: &models::Job) -> Result<(), QueueError> {
         println!("Consuming job: {:?}", job.id);
         println!("Consuming task: {:?}", job.task.id);
         println!("Payload: {:?}", job.task.payload);
@@ -25,7 +25,7 @@ mod tests {
     fn test_consume_returns_ok() {
         let consumer = JobConsumer;
         let job = make_test_job("job-1", "test payload");
-        let result = consumer.consume(job);
+        let result = consumer.consume(&job);
         assert!(result.is_ok());
     }
 
@@ -33,7 +33,7 @@ mod tests {
     fn test_consume_handles_empty_payload() {
         let consumer = JobConsumer;
         let job = make_test_job("job-2", "");
-        let result = consumer.consume(job);
+        let result = consumer.consume(&job);
         assert!(result.is_ok());
     }
 }
